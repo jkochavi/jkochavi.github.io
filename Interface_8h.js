4199564aee48ab9d99ff9c1f80e68beb70a84ca3
jkochavi <jdkochavi@comcast.net>
2021-03-09 18:59:51 -0800

    Updated documentation.
    Removed discussion about DC Buck, as the component was not longer necessary.

--- Interface_8h.js
+++ Interface_8h.js
@@ -1,8 +1,10 @@
 var Interface_8h =
 [
     [ "BOOST", "Interface_8h.html#ab97dfc84158d22d16e762f46c754176c", null ],
+    [ "CAN_ERROR", "Interface_8h.html#acf89dc0b59b9b73438c94f07495c7076", null ],
     [ "COAST", "Interface_8h.html#a20291e995fad7806c119183b4717f3f8", null ],
     [ "DIRECT", "Interface_8h.html#a86720ded03b3f5b5ca53d30b33cb33bb", null ],
+    [ "PEDAL", "Interface_8h.html#a00b776b8d8a7f1e9d409d44bb9a74fe6", null ],
     [ "REGEN", "Interface_8h.html#a11b3b506179458dd29a9fbded3921e05", null ],
     [ "task_CAN", "Interface_8h.html#a2799bc5582d595c9af523f6c05e59d03", null ],
     [ "task_display", "Interface_8h.html#a9e8d440b9f80517890c8ebe462ed9bfb", null ],
